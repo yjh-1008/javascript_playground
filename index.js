32ae0df59ed956ee4da53b4ef8dcad68f4b551cc
yjh1008 <exesea12@gmail.com>
2024-01-24 00:58:50 +0900

    feat: 이미지 로더 슬라이딩 구현

--- index.js
+++ index.js
@@ -13,41 +13,35 @@ class App{
   async onPrev() {
     if(this.page-1 <0) return;
     this.page-=1;
-    await this.imageLoad();
+    await this.imageLoad('right');
   }
   async onNext() {
     // if(this.page-1 <0) return;
     console.log('here');
     this.page+=1;
-    await this.imageLoad();
+    await this.imageLoad('left');
   }
 
-  async imageLoad() {
+  async imageLoad(dir='') {
     const imageLoader = document.querySelector('.image_container');
     const parent = document.querySelector('.parent');
-    imageLoader.classList.add('left');
-    // parent.removeChild(imageLoader);
-    // const nextImageLoader = document.createElement('div');
-    
-    // nextImageLoader.className='image_container';
-    // nextImageLoader.classList.add('left');
-    
-    // imageLoader = documment.createElement('div');
+    if(dir !== '')imageLoader.classList.add(dir);
     imageLoader.setAttribute("style","width:100%;max-height: 200px;display:flex");
     
     const {data} = await aixos.get(`https://picsum.photos/v2/list?page=${this.page}&limit=10`);
     imageLoader.replaceChildren();
     data.forEach((v,i) => {
       const img = document.createElement('img');
-      img.src = v.download_url;
       img.className = "list_img"
+      img.src = v.download_url;
+      img.loading="lazy";
       img.setAttribute("style","width:100px")
       img.addEventListener('click', () => {
         document.location.href= 'img.src';
       })
       imageLoader.appendChild(img);
     });
-    imageLoader.classList.remove('left');
+    if(dir !== '')imageLoader.classList.remove(dir);
     console.log(imageLoader.childNodes);
     // parent.appendChild(nextImageLoader);
   }
@@ -55,7 +49,6 @@ class App{
   async start() {
     const parent = document.querySelector('.parent');
     const prevBtn = document.querySelector('.prev');
-    parent.setAttribute("style","width:100%;max-height: 200px;display:flex")
     prevBtn.textContent='이전';
     prevBtn.addEventListener('click', () => this.onPrev());
     const nextBtn = document.querySelector('.forward');
